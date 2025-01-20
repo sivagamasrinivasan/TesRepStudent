fa987221146d030ecf159e00bae7055f696b3470
sivagamasrinivasan <784108@pdsb.net>
2025-01-20 17:21:43 -0500

    branch version 2

---
+++ src/examplestudent/PartTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examplestudent;
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+public class PartTimeStudent 
+{
+private int sid;    
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+}
